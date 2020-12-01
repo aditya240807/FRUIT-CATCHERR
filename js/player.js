55ace87ff84be82b550df5362ec7fbc7c3a16efc
aditya240807 <71052070+aditya240807@users.noreply.github.com>
2020-12-02 00:02:40 +0900

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,49 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+    getScore(){
+        var PlayerScoreRef = database.ref('playerScore')
+        PlayerScoreRef.on("value",(data)=>{
+            score1=data.val()
+        })
+    }
+    updateScore(score){
+       database.ref('/').update({
+           score1:score
+       })
+    }
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    
+}
